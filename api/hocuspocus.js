72aa706c9252af01ab342884d9ea8eeb0f3782b7
Vaisakh K M <vaisakhkm2625@gmail.com>
2024-06-22 05:23:48 +0530

    vercel edge function edge001

--- api/hocuspocus.js
+++ api/hocuspocus.js
@@ -1,14 +1,21 @@
 // api/hocuspocus.js
 
+//import { HocuspocusServer } from '@hocuspocus/server'
+//import { Server } from 'http'
+//import { createEdgeHandler } from '@vercel/node'
+//
+//const server = new HocuspocusServer()
+//
+//const handler = (req, res) => {
+//    // Handle incoming requests with Hocuspocus
+//    server.handle(req, res)
+//}
+//
+//export default createEdgeHandler(handler)
+
 import { HocuspocusServer } from '@hocuspocus/server'
-import { Server } from 'http'
-import { createEdgeHandler } from '@vercel/node'
+import { createEdgeHandler } from '@vercel/edge'
 
 const server = new HocuspocusServer()
 
-const handler = (req, res) => {
-    // Handle incoming requests with Hocuspocus
-    server.handle(req, res)
-}
-
-export default createEdgeHandler(handler)
+export default createEdgeHandler(server.handle.bind(server))
