f4e7d2fee0c4896f1d2d66d9de3e950339d81218
Vaisakh K M <vaisakhkm2625@gmail.com>
2024-06-22 05:43:39 +0530

    vercel adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+//
+///** @type {import('@sveltejs/kit').Config} */
+//const config = {
+//	kit: {
+//		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+//		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+//		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+//		adapter: adapter()
+//	}
+//};
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+import adapter from '@sveltejs/adapter-vercel';
+
+export default {
+    kit: {
+        adapter({
+            images: {
+                sizes: [640, 828, 1200, 1920, 3840],
+        formats: ['image/avif', 'image/webp'],
+        minimumCacheTTL: 300,
+        domains: ['example-app.vercel.app'],
+    }
+})
+    }
 };
-
-export default config;
